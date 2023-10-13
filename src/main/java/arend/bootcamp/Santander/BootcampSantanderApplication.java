e8022b1f2feb4bfb8f7f7089d5f89be73a4dcf9d
Aline <aline.arend04@gmail.com>
2023-10-13 07:00:28 -0300

    Initial commit

---
+++ src/main/java/arend/bootcamp/Santander/BootcampSantanderApplication.java
@@ -0,0 +1,13 @@
+package arend.bootcamp.Santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arend/bootcamp/Santander/BootcampSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package arend.bootcamp.Santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
